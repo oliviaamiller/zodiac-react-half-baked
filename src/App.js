2634d27efb2c538fd3119a08e6535cd3a7aa4fcb
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-24 14:33:17 -0800

    app html components have strings and zodiac array

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css'
 import Footer from './Footer/Footer.js'
 import Main from './Main/Main.js'
 import Header from './Header/Header.js'
+import { zodiac } from './data.js'
 {/* import the zodiac array */}
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      {/* import and use the Header, Main, and Footer components here */}
+      {/* import and use the Header, Main, and Footer components here */
+        <><Header name={props.name} />
+          <Footer year={props.year}/>
+          <Main zodiacs={zodiac}/>
+        </>
+      }
       {/* the Header component needs a string passed in as the `name` prop */}
-      <Header />
+      
       
       {/* the Footer component needs a year passed in as the `year` prop */}
-      <Footer />
+      
 
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
-      <Main />
-      <h1>hello!</h1>
+      
     </div>
   )
 }
