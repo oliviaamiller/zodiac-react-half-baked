d79f9a2827bb694a6e0cd7991a50c705e39cd577
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-24 15:21:14 -0800

    zodiac name style and images displaying

--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -2,12 +2,11 @@ import './ZodiacCard.css'
 
 export default function ZodiacCard(props) {
   {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */
-    return <p>
-      {props.name}
+    return <div className='zodiac-card'>
+      <img src={ `/images/${props.name}.png` } />
+      <p className='name'>{props.name}</p>
       {props.dates}
-      
-    </p>
+    </div >
   }
 }
 
-// src={ `/images/${props.name}.png` }
